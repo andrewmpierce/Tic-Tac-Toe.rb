0404c772dd7eeafbe6670d4edf4c46146ccccdd0
apierce1992 <apierce1992@gmail.com>
2015-12-08 14:31:24 -0500

    added scaffold classes and test scaffold

---
+++ tests.rb
@@ -0,0 +1,17 @@
+require_relative "player"
+require_relative "board"
+require "test/unit"
+
+class TestPlayer < Test::Unit::TestCase
+
+  #This is a template test I will be referring to
+  # def test_simple
+  #   assert_equal(4, SimpleNumber.new(2).add(2) )
+  #   assert_equal(6, SimpleNumber.new(2).multiply(3) )
+  # end
+
+end
+
+class TestBoard < Test::Unit::TestCase
+
+end
